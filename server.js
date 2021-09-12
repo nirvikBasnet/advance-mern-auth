fb7fc4c43bf4dd369367835f3311f21735cdcebe
nirvikBasnet <nirvik41@gmail.com>
2021-09-12 18:25:44 +1000

    mongoDB connected, bcrypt added and created some routes

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-require('dotenv').config({ path: "./config.env" })
+require('dotenv').config({ path: "./config.env" });
 const express = require('express');
+const connectDB = require('./config/db');
+
+// Connect to Mongo
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+
+app.use('/api/auth', require('./routes/auth'));
+
 const PORT = process.env.PORT | 8000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Logged Error : ${err}`);
+    server.close(() => process.exit(1));
+})
 
